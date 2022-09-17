b4955cfcb8ae889860b844c0479bb3de97cd7ab1
Fazila <fazilamujawar777@gmail.com>
2022-09-17 10:13:58 +0530

    Listener added

--- src/test/java/com/utility/ListenerClass.java
+++ src/test/java/com/utility/ListenerClass.java
@@ -3,15 +3,14 @@ package com.utility;
 import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
-import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class ListenerClass implements ITestListener {
+	public class ListenerClass implements ITestListener {
 	
 	CaptureWindow cw=new CaptureWindow();
 	
-public void onTestStart(ITestResult result) 
+	public void onTestStart(ITestResult result) 
 	{	
 		System.out.println("Test started");
 	}
